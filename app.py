1f0fa76227eb60925173f4fcafc5fea735bc5ff4
c-pompa <59244901+c-pompa@users.noreply.github.com>
2020-06-09 15:14:52 -0700

    app.py & climate_analysis completed. Needs verification on small things. updated gitignore.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
 #################################################
@@ -31,7 +31,6 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-
 
 @app.route("/")
 #   * Home page.
@@ -131,18 +130,163 @@ def Temp_Obs():
 # #   * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
 # def passengers():
 
+# This function called `calc_temps` will accept start date and end date in the format '%Y-%m-%d' 
+# and return the minimum, average, and maximum temperatures for that range of dates
+
+@app.route("/api/v1.0/<start_date>")
+def Start_Date_Search(start_date):
+    """Fetch the beginning date requested when 'start' matches
+       the path variable supplied by the user, or a 404 if not."""
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+
+    # Fetch latest date in dataset
+    latest_date = (session.query(Measurement.date)
+               .order_by(Measurement.date.desc()).first())
+    latest_date = ''.join(map(str, latest_date))
+
+    try:
+        # Verify date input matches date in dataset
+        input_date = (session.query(Measurement.date)
+        .filter(Measurement.date == start_date).first())
+        input_date = ''.join(map(str, input_date))
+    except:
+        return jsonify({"error": f"{start_date} not found. Date given is not located in database or wrong format. Date format is 'YYYY-MM-DD'."}), 404
+        
+
+    def calc_temps(start_date):
+        """TMIN, TAVG, and TMAX for a list of dates.
+        Args:
+            start_date (string): A date string in the format %Y-%m-%d
+            end_date (string): A date string in the format %Y-%m-%d
+            
+        Returns:
+            TMIN, TAVE, and TMAX
+            
+        """
+        return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= latest_date).all()
+
+    session.close()
+
+    # Get input, lower text for matching purposes. Date may not apply but input anyways
+    canonicalized = start_date.replace(" ", "").lower()
+    
+    # Verify input with database
+    search_term = input_date.replace(" ", "").lower()
+
+
+    if search_term == canonicalized:
+        results = calc_temps(start_date)
+
+        return (
+            f'<h1>Temperature Observation Minumum, Average, and Maximum for date range {search_term} to {latest_date}:</h1><br/>'
+            f'Minumum - Average - Maximum<br/>'
+            f'{results}')
+
+
+    else:
+        return jsonify({"error": f"{start_date} not found. Date given is not located in database or wrong format. Date format is 'YYYY-MM-DD'."}), 404
+       
+    
+
 
 
 # @app.route("/api/v1.0/<start>/<end>")
 # # * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 # #   * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
 # #   * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
-# def passengers():
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def End_Date_Search(start_date, end_date):
+    """Fetch the beginning date requested when 'start' matches
+       the path variable supplied by the user, or a 404 if not."""
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+
+    try:
+        # Verify date input matches date in dataset
+        input_date = (session.query(Measurement.date)
+        .filter(Measurement.date == start_date).first())
+        input_date = ''.join(map(str, input_date))
+    except:
+        return jsonify({"error": f"{start_date} not found. Date given is not located in database or wrong format. Date format is 'YYYY-MM-DD'."}), 404
+        
+    try:
+        # Verify date input matches date in dataset
+        input_end_date = (session.query(Measurement.date)
+        .filter(Measurement.date == end_date).first())
+        input_end_date = ''.join(map(str, input_end_date))
+    except:
+        return jsonify({"error": f"{end_date} not found. Date given is not located in database or wrong format. Date format is 'YYYY-MM-DD'."}), 404
+           
+
+    def calc_temps(start_date, end_date):
+        """TMIN, TAVG, and TMAX for a list of dates.
+        Args:
+            start_date (string): A date string in the format %Y-%m-%d
+            end_date (string): A date string in the format %Y-%m-%d
+            
+        Returns:
+            TMIN, TAVE, and TMAX
+            
+        """
+        return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+
+
+    session.close()
+
+
+    # Get input, lower text for matching purposes. Date may not apply but input anyways
+    canonicalized = start_date.replace(" ", "").lower()
+    canonicalized_end = end_date.replace(" ", "").lower()
+
+    # Verify input with database
+    search_term = input_date.replace(" ", "").lower()
+    search_term_end = input_end_date.replace(" ", "").lower()
+
+    # (search_term, search_term_end) = (canonicalized, canonicalized)
+
+    if (search_term, search_term_end) == (canonicalized, canonicalized_end):
+        results = calc_temps(start_date, end_date)
+
+        return (
+            f'<h2>Temperature Observation Minumum, Average, and Maximum for date range {search_term} to {search_term_end}:</h1><br/>'
+            f'Minumum - Average - Maximum<br/>'
+            f'{results}')
+
+
+    else:
+        return jsonify({"error": f"Start and End date provided do not match after verifying with the database. Start: {start_date}, End: {end_date}."}), 404
+        # return (f'passed canonicalized {canonicalized}, passed search_term {search_term}, NOT results')
+    
+
+
+
+
+
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
